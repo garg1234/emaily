32446188ede3d74d05674639b6be2b3b3a98ef91
Ankit Garg <gargankit1061997@gmail.com>
2020-09-10 01:59:08 +0530

    Final Commit

---
+++ routes/surveyRoutes.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+const requireCredits = require("../middlewares/requireCredits");
+
+const Survey = mongoose.model("surveys");
+
+module.exports = (app) => {
+  app.get("/api/surveys", requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recepients: false,
+    });
+    res.send(surveys);
+  });
+
+  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients
+        .split(",")
+        .map((email) => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now(),
+    });
+    try {
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
+  });
+};
